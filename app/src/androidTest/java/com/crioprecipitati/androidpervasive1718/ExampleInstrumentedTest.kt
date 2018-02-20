9401a083b1f2eab8c8768cfbeae95e113992ff23
michelecampobasso <michele.campobasso3@gmail.com>
2018-02-20 14:01:34 +0100

    Initial commit, created new project

---
+++ app/src/androidTest/java/com/crioprecipitati/androidpervasive1718/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.crioprecipitati.androidpervasive1718
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.crioprecipitati.androidpervasive1718", appContext.packageName)
+    }
+}
