17c4ce55071faed3c91da6032b4fd450072b0be6
michelecampobasso <michele.campobasso3@gmail.com>
2018-02-21 16:25:13 +0100

    Partials

--- app/src/main/java/com/crioprecipitati/androidpervasive1718/viewPresenter/login/LoginPresenterImpl.kt
+++ app/src/main/java/com/crioprecipitati/androidpervasive1718/viewPresenter/login/LoginPresenterImpl.kt
@@ -1,6 +1,5 @@
 package com.crioprecipitati.androidpervasive1718.viewPresenter.login
 
-import android.support.design.widget.Snackbar
 import com.crioprecipitati.androidpervasive1718.networking.handlers.SessionApiHandler
 import io.reactivex.schedulers.Schedulers
 
@@ -17,22 +16,33 @@ class LoginPresenterImpl : LoginContract.LoginPresenter {
     }
 
     override fun onConnectRequested(cf: String, memberType: MemberType) {
-        val subscription = SessionApiHandler().getAllSessions()
+        SessionApiHandler().getAllSessions()
             .subscribeOn(Schedulers.io())
             .subscribe (
-                    { retrievedSessions ->
-                        //(session_list.adapter as SessionAdapter).addSession(retrievedSessions)
-                        println(retrievedSessions.first())
-                    },
-                    { e ->
-                        //Snackbar.make(session_list, e.message ?: "", Snackbar.LENGTH_LONG).show()
-                        println(e.message)
-                    }
+                { retrievedSessions ->
+                    //(session_list.adapter as SessionAdapter).addSession(retrievedSessions)
+                    println(retrievedSessions.first())
+                },
+                { e ->
+                    //Snackbar.make(session_list, e.message ?: "", Snackbar.LENGTH_LONG).show()
+                    println(e.message)
+                }
             )
     }
 
     override fun onNewSessionRequested() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    /*    SessionApiHandler().createNewSession()
+                .subscribeOn(Schedulers.io())
+                .subscribe (
+                        { retrievedSessions ->
+                            //(session_list.adapter as SessionAdapter).addSession(retrievedSessions)
+                            println(retrievedSessions.first())
+                        },
+                        { e ->
+                            //Snackbar.make(session_list, e.message ?: "", Snackbar.LENGTH_LONG).show()
+                            println(e.message)
+                        }
+                )*/
     }
 
     override fun onSessionSelected() {
