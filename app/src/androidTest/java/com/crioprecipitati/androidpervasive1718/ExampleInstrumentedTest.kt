263bf3a1c34d55f9438dfdb691fdea4ffa4d9263
Edoardo Antonini <edoardo.antonini@studio.unibo.it>
2018-02-28 16:26:43 +0100

    Test put in Android Test

--- app/src/androidTest/java/com/crioprecipitati/androidpervasive1718/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/crioprecipitati/androidpervasive1718/ExampleInstrumentedTest.kt
@@ -1,24 +1,37 @@
 package com.crioprecipitati.androidpervasive1718
 
-import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.hasDescendant
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.runner.AndroidJUnit4
-
+import android.support.test.rule.ActivityTestRule
+import com.crioprecipitati.androidpervasive1718.viewPresenter.login.LoginActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ApplicationTest {
+    @Rule
+    @JvmField
+    val activity = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.crioprecipitati.androidpervasive1718", appContext.packageName)
+    fun startNewSession(){
+        val stringToBeTyped = "CFLeader"
+        onView(withId(R.id.rbLeader)).perform(click())
+        onView(withId(R.id.etUsername)).perform(clearText()).perform(typeText(stringToBeTyped)).perform(closeSoftKeyboard())
+        onView(withId(R.id.btnCreateNewSession)).perform(click())
+        onView(withId(R.id.rbMember)).perform(click())
+        onView(withId(R.id.btnRequestOpenSessions)).perform(click())
+        Thread.sleep(4000)
+        onView(withId(R.id.rvSessionList))
+                .check(matches(hasDescendant(withText(stringToBeTyped))))
     }
+
 }
