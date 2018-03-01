0f6e812d765c40151f209edce1d274ea534caaff
Edoardo Antonini <edoardo.antonini@studio.unibo.it>
2018-03-01 12:43:30 +0100

    FINISHED LoginActivityTest

--- app/src/androidTest/java/com/crioprecipitati/androidpervasive1718/AndroidTest.kt
+++ app/src/androidTest/java/com/crioprecipitati/androidpervasive1718/AndroidTest.kt
@@ -8,12 +8,14 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.crioprecipitati.androidpervasive1718.viewPresenter.login.LoginActivity
 import org.hamcrest.core.StringContains.containsString
-import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.hamcrest.CoreMatchers.not
-import org.junit.Before
+import android.support.test.InstrumentationRegistry
+import android.support.test.uiautomator.UiDevice
+
+//TODO vedere se sostituire gli sleep con degli idling
 
 @RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
@@ -30,41 +32,43 @@ class LoginActivityTest {
 
 
     }
-    @Before
-    fun setup() {
+
+    fun createSessions(){
+        val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         onView(withId(R.id.rbLeader)).perform(click())
         onView(withId(R.id.etUsername)).perform(clearText()).perform(typeText(leaderCF1)).perform(closeSoftKeyboard())
         onView(withId(R.id.etPatient)).perform(clearText()).perform(typeText(patientCF1)).perform(closeSoftKeyboard())
         onView(withId(R.id.btnCreateNewSession)).perform(click())
-        Thread.sleep(1000)
-        pressBack()
+        Thread.sleep(10000)
+        mDevice.pressBack()
         onView(withId(R.id.etPatient)).perform(clearText()).perform(typeText(patientCF2)).perform(closeSoftKeyboard())
         onView(withId(R.id.btnCreateNewSession)).perform(click())
-        Thread.sleep(1000)
-        pressBack()
+        Thread.sleep(10000)
+        mDevice.pressBack()
         onView(withId(R.id.etUsername)).perform(clearText()).perform(typeText(leaderCF2)).perform(closeSoftKeyboard())
         onView(withId(R.id.etPatient)).perform(clearText()).perform(typeText(patientCF1)).perform(closeSoftKeyboard())
         onView(withId(R.id.btnCreateNewSession)).perform(click())
-        Thread.sleep(1000)
-        pressBack()
+        Thread.sleep(10000)
+        mDevice.pressBack()
         onView(withId(R.id.etPatient)).perform(clearText()).perform(typeText(patientCF2)).perform(closeSoftKeyboard())
         onView(withId(R.id.btnCreateNewSession)).perform(click())
-        Thread.sleep(1000)
-        pressBack()
+        Thread.sleep(10000)
+        mDevice.pressBack()
     }
 
     @Test
     fun sessionListByLeader(){
+        createSessions()
         onView(withId(R.id.etUsername)).perform(clearText()).perform(typeText(leaderCF1)).perform(closeSoftKeyboard())
         onView(withId(R.id.btnRequestOpenSessions)).perform(click())
-        Thread.sleep(1000) //TODO vedere se farlo con gli idling
+        Thread.sleep(1000)
         onView(withId(R.id.rvSessionList))
                 .check(matches(hasDescendant(withText(containsString(leaderCF1)))))
                 .check(matches(not(hasDescendant(withText(containsString(leaderCF2))))))
                 .check(RecyclerViewItemCountAssertion(2))
         onView(withId(R.id.etUsername)).perform(clearText()).perform(typeText(leaderCF2)).perform(closeSoftKeyboard())
         onView(withId(R.id.btnRequestOpenSessions)).perform(click())
-        Thread.sleep(1000) //TODO vedere se farlo con gli idling
+        Thread.sleep(1000)
         onView(withId(R.id.rvSessionList))
                 .check(matches(hasDescendant(withText(containsString(leaderCF2)))))
                 .check(matches(not(hasDescendant(withText(containsString(leaderCF1))))))
@@ -73,8 +77,9 @@ class LoginActivityTest {
 
     @Test
     fun sessionListByMember(){
+        onView(withId(R.id.rbMember)).perform(click())
         onView(withId(R.id.btnRequestOpenSessions)).perform(click())
-        Thread.sleep(1000) //TODO vedere se farlo con gli idling
+        Thread.sleep(1000)
         onView(withId(R.id.rvSessionList))
             .check(matches(hasDescendant(withText(containsString(leaderCF1)))))
             .check(matches(hasDescendant(withText(containsString(leaderCF2)))))
