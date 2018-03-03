8a83776aaa270e5de701c7b33d5029372971a803
Edoardo Antonini <edoardo.antonini@studio.unibo.it>
2018-03-03 16:00:53 +0100

    FIXED task data class

--- app/src/main/java/com/crioprecipitati/androidpervasive1718/model/Entities.kt
+++ app/src/main/java/com/crioprecipitati/androidpervasive1718/model/Entities.kt
@@ -37,7 +37,7 @@ class AugmentedMember(val userCF: String, items: MutableList<AugmentedTask> = mu
 }
 
 @Parcelize
-data class Task @JvmOverloads constructor(val id: Int = 0, val sessionId: Int, val userCF: String, @KlaxonDate val startTime: Timestamp, @KlaxonDate val endTime: Timestamp, val activityId: Int, var statusId: Int) : Parcelable {
+data class Task(val id: Int = 0, val sessionId: Int, val operatorCF: String, @KlaxonDate val startTime: Timestamp, @KlaxonDate val endTime: Timestamp, val activityId: Int, var statusId: Int): Parcelable {
     companion object {
         fun emptyTask(): Task =
                 Task(EmptyTask.emptyTaskId, EmptyTask.emptySessionId, EmptyTask.emptyTaskOperatorId, EmptyTask.emptyTaskStartTime, EmptyTask.emptyTaskEndTime, EmptyTask.emptyTaskActivityId, EmptyTask.emptyTaskStatusId)
