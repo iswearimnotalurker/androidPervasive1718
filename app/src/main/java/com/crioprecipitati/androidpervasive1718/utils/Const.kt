9f8021c4e6a52788c09c6cf9c2b56e30ff69d41d
albertogiunta <alb.giunta@gmail.com>
2018-04-05 10:42:40 +0200

    ADDED dynamic ip

--- app/src/main/java/com/crioprecipitati/androidpervasive1718/utils/Const.kt
+++ app/src/main/java/com/crioprecipitati/androidpervasive1718/utils/Const.kt
@@ -1,13 +1,5 @@
 package com.crioprecipitati.androidpervasive1718.utils
 
-
-const val IP_ALBERTO = "192.168.1.102"
-const val IP_MICHELE = "192.168.1."
-const val IP_EDOARDO = "192.168.1.102"
-const val IP_MATTEO = "192.168.0.8"
-const val IP_ALESSANDRO = "192.168.1.10"
-const val IP_UNIVERSITA = "192.168.43.179"
-
 var CURRENT_LOCAL_IP = Prefs.ip
 
 val WS_DEFAULT_TASK_URI = "ws://$CURRENT_LOCAL_IP:820${Prefs.instanceId}/task"
